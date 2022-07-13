61736f61441cb5fcc16fec667107bf3c699bf730
hizashi <zytwp@hotmail.com>
2022-07-13 16:48:13 +0800

    added metrics for latest block time in unix

--- node_status_prom_file.py
+++ node_status_prom_file.py
@@ -31,14 +31,16 @@ latest_block_time = datetime.fromisoformat(latest_block_time_string)
 current_system_time = datetime.utcnow()
 delta_seconds = (current_system_time - latest_block_time).total_seconds()
 
+latest_block_time_unix = latest_block_time.timestamp() * 1000
+
 catching_up: bool = parse_check_status["SyncInfo"]["catching_up"]
 if catching_up:
     sync_status = 1
 else:
     sync_status = 0
 
-metrics_list = ["# HELP latest_block_height The latest block height in this node.", "# TYPE latest_block_height counter", "latest_block_height %s" % latest_block_height, "# HELP delta_seconds Difference in seconds between system time and latest block time.",
-                "# TYPE delta_seconds gauge", "delta_seconds %.2f" % delta_seconds, "# HELP sync_status This references the 'catching_up' boolean of the node. 0 for False and 1 for True.", "# TYPE sync_status gauge", "sync_status %d" % sync_status]
+metrics_list = ["# HELP latest_block_height The latest block height in this node.", "# TYPE latest_block_height counter", "latest_block_height %s" % latest_block_height, "# HELP delta_seconds Difference in seconds between system time and latest block time.", "# TYPE delta_seconds gauge", "delta_seconds %.2f" % delta_seconds,
+                "# HELP sync_status This references the 'catching_up' boolean of the node. 0 for False and 1 for True.", "# TYPE sync_status gauge", "sync_status %d" % sync_status, "# HELP latest_block_time_unix The latest block time in this node, in unix (miliseconds).", "# TYPE latest_block_time_unix counter", "latest_block_time_unix %d" % latest_block_time_unix]
 
 with open(METRICS_FILE_NAME, 'w') as f:
     for line in metrics_list:
