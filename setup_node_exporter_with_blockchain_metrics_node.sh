ca6bdf6b09ab6bf2d678adf5d67f752560747ea5
hizashi <zytwp@hotmail.com>
2023-03-06 15:20:48 +0800

    add node exporter script for vnode

---
+++ setup_node_exporter_with_blockchain_metrics_node.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Use this for archival, full, prune node.
+# Requires one chain specific command as argument.
+# Downloads node_exporter from prometheus.
+# Downloads node_status_prom_script.py that serves a .prom file for the node_exporter.
+# Sets up a cronjob to refresh the .prom file every minute.
+# Starts the node_exporter in a tmux session
+
+set -e
+
+if [ $# -lt 1 ]; then
+    echo "Requires one argument. Argument is the chain specific command."
+    exit 1
+elif [ $# -gt 1 ]; then
+    echo "Please specify only one argument."
+    exit 1
+fi
+
+node_exporter_directory=node_exporter-1.5.0.linux-amd64
+prom_script_file=node_status_prom_script.py
+
+echo "Changing to home directory..."
+cd
+echo "Downloading $node_exporter_directory"
+if [ -d "$node_exporter_directory" ]; then
+    rm -rf $node_exporter_directory
+fi
+wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz -P ~ 
+echo "Extracting file..."
+tar -xzf node_exporter-1.5.0.linux-amd64.tar.gz
+rm -f node_exporter-1.5.0.linux-amd64.tar.gz
+echo "$node_exporter_directory downloaded."
+
+echo "Downloading node_status_prom_script.py"
+if [ -f "$prom_script_file" ]; then
+    rm -f $prom_script_file
+fi
+wget https://raw.githubusercontent.com/coinhall/os-scripts/main/node_status_prom_script.py -P ~
+echo "$prom_script_file downloaded."
+
+echo "Setting up cronjob..."
+(crontab -l || true; echo "*/1 * * * * python3 ~/node_status_prom_script.py $1 > ./node_exporter_cron.log 2>&1") | awk '!seen[$0]++' | crontab -
+echo "cronjob set up done."
+
+echo "Starting node_exporter in tmux session called node-exporter..."
+session_name=node-exporter
+session_exist=$(tmux ls | grep $session_name) || session_exist=""
+if [ ! "$session_exist" = "" ]; then
+    tmux kill-session -t $session_name
+fi
+tmux new-session -d -s $session_name
+tmux send-keys -t $session_name 'cd node_exporter-1.5.0.linux-amd64' C-m
+tmux send-keys -t $session_name './node_exporter --collector.textfile.directory="$HOME"' C-m
+echo "node_exporter started"
--- setup_node_exporter_with_blockchain_metrics_v.sh
+++ setup_node_exporter_with_blockchain_metrics_v.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Use this for validator node.
 # Requires one chain specific command as argument.
 # Downloads node_exporter from prometheus.
 # Downloads node_status_prom_script.py that serves a .prom file for the node_exporter.
@@ -16,7 +17,7 @@ elif [ $# -gt 1 ]; then
 fi
 
 node_exporter_directory=node_exporter-1.5.0.linux-amd64
-prom_script_file=node_status_prom_script.py
+prom_script_file=vnode_status_prom_script.py
 
 echo "Changing to home directory..."
 cd
@@ -30,15 +31,15 @@ tar -xzf node_exporter-1.5.0.linux-amd64.tar.gz
 rm -f node_exporter-1.5.0.linux-amd64.tar.gz
 echo "$node_exporter_directory downloaded."
 
-echo "Downloading node_status_prom_script.py"
+echo "Downloading vnode_status_prom_script.py"
 if [ -f "$prom_script_file" ]; then
     rm -f $prom_script_file
 fi
-wget https://raw.githubusercontent.com/coinhall/os-scripts/main/node_status_prom_script.py -P ~
+wget https://raw.githubusercontent.com/coinhall/os-scripts/main/vnode_status_prom_script.py -P ~
 echo "$prom_script_file downloaded."
 
 echo "Setting up cronjob..."
-(crontab -l || true; echo "*/1 * * * * python3 ~/node_status_prom_script.py $1 > ./node_exporter_cron.log 2>&1") | awk '!seen[$0]++' | crontab -
+(crontab -l || true; echo "*/1 * * * * python3 ~/vnode_status_prom_script.py $1 > ./node_exporter_cron.log 2>&1") | awk '!seen[$0]++' | crontab -
 echo "cronjob set up done."
 
 echo "Starting node_exporter in tmux session called node-exporter..."
