7b3007351eec0ae076e37b31dbd557e185e68611
Aaron Choo <choozy.aaron@gmail.com>
2023-09-06 14:16:24 +0800

    chore: update ch installation

--- setup_ch.sh
+++ setup_ch.sh
@@ -32,8 +32,11 @@ echo "OS configurations successfully updated!"
 # See: https://clickhouse.com/docs/en/install#install-from-deb-packages
 echo "Installing ClickHouse..."
 sudo apt-get install -y apt-transport-https ca-certificates dirmngr
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 8919F6BD2B48D754
-echo "deb https://packages.clickhouse.com/deb stable main" >/etc/apt/sources.list.d/clickhouse.list
+GNUPGHOME=$(mktemp -d)
+sudo GNUPGHOME="$GNUPGHOME" gpg --no-default-keyring --keyring /usr/share/keyrings/clickhouse-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 8919F6BD2B48D754
+sudo rm -r "$GNUPGHOME"
+sudo chmod +r /usr/share/keyrings/clickhouse-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/clickhouse-keyring.gpg] https://packages.clickhouse.com/deb stable main" | sudo tee /etc/apt/sources.list.d/clickhouse.list
 sudo apt-get update
 sudo apt-get install -y clickhouse-server clickhouse-client
 sudo service clickhouse-server start
